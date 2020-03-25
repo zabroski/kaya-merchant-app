f64082f43c5940277a225fb06d0e6e75d0c9dad2
Issouf Zabre <sheridanzabre@gmail.com>
2020-03-25 17:11:43 -0400

    auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import  React  from 'react';
 import {Route, Link} from "react-router-dom";
 import  ProtectedRoute from './components/ProtectedRoute';
+import DashboardPage from './components/DashboardPage';
 import SignupPage from './components/SignupPage';
 import LoginPage from  './components/LoginPage';
 import authService from './lib/authService';
@@ -12,25 +13,56 @@ class App extends React.Component {
     super(props)
     this.state = {
       isSigneIn: false,
-      merchat: {}
+      merchant: {}
     }
 
   }
+
+  signOutDeliverere = () => {
+    authService.signOut()
+  
+    this.setState(state => {
+      return {
+        isSignedIn: false,
+        deliverer: {}
+      }
+    })
+  }
+
+
+
   render() {
+
+    const { merchant } = this.state;
+
     return (
       <div className="App">
          {authService.isAuthenticated()}
          <nav className="nav-menu">
-            <Link>REGISTER</Link>
-            <Link>Dashboard</Link>
+            {authService.isAuthenticated() && <Link to='/'>Dashboard</Link>}
+            {!authService.isAuthenticated() ?  <Link to="/login">SIGN IN</Link> :
+              <button className="signUp-button" onClick= {this.signOutDeliverere}>Sign Out</button>}
+
+              {!authService.isAuthenticated() ? (<Link to="/signup">REGISTER</Link>) : (null)} 
          </nav>
-        {/* <h1>Merchant</h1> */}
-
-        
-        {/* <SignupPage /> */}
-        <LoginPage />
-
-     
+         <main className="main-menu">
+            <Route 
+                exact={true}
+                path="/login"
+                render={(props) => <LoginPage  {...props}/>} 
+              />
+              <Route 
+                exact={true}
+                path='/signup' 
+                render = {(props) => <SignupPage {...props} />}
+              />
+              <ProtectedRoute 
+                exact={true}
+                path='/'
+                merchant={merchant} 
+                component={DashboardPage}
+              />
+         </main>
       </div>
     );
   }
